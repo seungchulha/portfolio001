db53d7d308d5b3503101ab9bd5685acdb348ad5c
Seungchul Ha <seungchulha@Seungchuls-MacBook-Pro.local>
2021-04-19 16:24:12 +0900

    환경세팅완료 v1

---
+++ src/main/java/com/example/portfolio001/Portfolio001Application.java
@@ -0,0 +1,13 @@
+package com.example.portfolio001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Portfolio001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfolio001/Portfolio001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
